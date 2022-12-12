fd302fe8e382a876e0952c7870c23ddf43a7372b
Malaz-22 <malazm09@gmail.com>
2022-12-12 11:33:08 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - body of program
+ *
+ * Return: 0 to be correct
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
