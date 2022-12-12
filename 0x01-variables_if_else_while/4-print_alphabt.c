ca2b7ff7018a6686261f143e95f9d7a57a1a3c54
Malaz-22 <malazm09@gmail.com>
2022-12-12 09:52:01 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this basic of program
+ *
+ * Return: is 0 to be correct
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	if (a == 'e' || a == 'q')
+	continue;
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
