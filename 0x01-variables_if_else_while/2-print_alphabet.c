76c3ba88bcff8788ec3120026040d4f863521509
Malaz-22 <malazm09@gmail.com>
2022-12-12 03:40:52 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - body of program
+ * here code begins
+ * Return: 0 to be correct
+ */
+char main(void)
+{
+	char a;
+
+	for
+		('a'; a < 'z'; a++)
+		putchar(a);
+		return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/** main - body of program
+ * here begin excuite code
+ * Return: 0 to be correct
+ */
+char main(void)
+{
+
