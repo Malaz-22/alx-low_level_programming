55a4dcb8692686c918bce898cde76fc4d072a1d4
Malaz-22 <malazm09@gmail.com>
2022-12-12 11:57:15 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - body of program
+ *
+ * Return: 0 to be correct
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a <= 'z'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
