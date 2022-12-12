b534b589098b442dbce5fbf4a3a85fff6b3a8276
Malaz-22 <malazm09@gmail.com>
2022-12-12 10:41:09 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - bogy of program
+ *
+ * Return: 0 to be correct
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
