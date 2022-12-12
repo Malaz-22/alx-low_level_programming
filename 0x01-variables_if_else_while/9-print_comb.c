6e310c89b178a4e6b782af0990c0bc869163ddc4
Malaz-22 <malazm09@gmail.com>
2022-12-12 13:24:26 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - body of code
+ *
+ * Return: 0 to be correct
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+		if (x != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
