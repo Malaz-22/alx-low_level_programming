1fb530ae1f46e8fc481071ef757784259df01e5d
Malaz-22 <malazm09@gmail.com>
2022-12-13 14:47:48 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print alphapet in lower case
+ *
+ * Return: unfound
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
