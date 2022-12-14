3a1c4bbb2a109b5070bde27c591f517b656fa9dd
Malaz-22 <malazm09@gmail.com>
2022-12-14 08:54:54 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of number
+ * @n: the number
+ * Return: value of last digit
+ */
+int print_last_digit(int n)
+{
+	n = 0;
+	while (n >= 9)
+	{
+		return (n);
+		n++;
+	}
+}
