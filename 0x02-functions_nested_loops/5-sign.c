ed66da7385eaa13c2c2f8b5a5b8fe895ab58eb68
Malaz-22 <malazm09@gmail.com>
2022-12-14 07:22:15 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - test the sign of number
+ * @n: number we will signed
+ * Return: 1 if n positive
+ * Return; -1 if n negative
+ * otherwise return 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar(0);
+		return (0);
+	}
+}
