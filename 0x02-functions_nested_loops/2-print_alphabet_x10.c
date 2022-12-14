05b9de277ed2e6527b27fbb554555ad240882b75
Malaz-22 <malazm09@gmail.com>
2022-12-13 16:00:55 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alpabet 10 times
+ *
+ * Return: not found
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int i;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+	i = 0;
+	while (i < 10)
+	{
+	_putchar('\n');
+	i++;
+	}
+	_putchar(a);
+	}
+}
