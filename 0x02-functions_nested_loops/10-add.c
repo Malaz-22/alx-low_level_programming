26755a789bde7221c48dac0f76e9a224f0d48c63
Malaz-22 <malazm09@gmail.com>
2022-12-14 11:03:05 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add numbers to give result
+ * @n: number 1
+ * @j: number 2
+ * Return: the add result
+ */
+int add(int n, int j)
+{
+	int result;
+
+	result = n + j;
+	return (result);
+}
