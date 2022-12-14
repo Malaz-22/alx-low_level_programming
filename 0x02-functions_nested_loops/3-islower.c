ec294f6b0085512b4269e0fcacea2df3402cd91f
Malaz-22 <malazm09@gmail.com>
2022-12-14 03:09:12 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lower case character
+ *
+ * Return: 1 if result of character is lower case
+ * Return: 0 if false result
+ */
+int _islower(int c)
+{
+	return (c);
+}	
