1e67ad2d88c112fda7b02aa987f5b8ab14975c41
Malaz-22 <malazm09@gmail.com>
2022-12-14 12:29:37 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,15 +4,16 @@
 /**
  * print_to_98 - print until 98
  * @n: number
- * Return: no return value (void)
+ * no return value (void)
  */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n > 0)
+	if (n <= 98)
 	{
 		_putchar(n + '0');
 		_putchar(',');
 		_putchar(' ');
-		n++;
 	}
+	_putchar('\n');
 }
