7ff9617a2c9fd68ef99c5f794c78e4b14c7fa198
Malaz-22 <malazm09@gmail.com>
2022-12-13 16:41:35 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,14 @@
 void print_alphabet_x10(void)
 {
 	char a;
-	int i;
+	int i = 0;
 
-	for (a = 'a'; a <= 'z'; a++)
-	{
-	i = 0;
 	while (i < 10)
 	{
+	for (a = 'a'; a <= 'z'; a++)
+	{
 	_putchar(a);
-	i++;
 	}
-	_putchar(a);
+	_putchar('\n');
 	}
 }
