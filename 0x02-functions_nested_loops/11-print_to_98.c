5c0553e50cabcc68529d0b170bb3aa4c87529f0a
Malaz-22 <malazm09@gmail.com>
2022-12-15 01:37:19 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,25 +8,15 @@
  */
 void print_to_98(int n)
 {
-	int i, j
+	int i;
 
-	if (n <= 98)
+	if (n > 0)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (i != 98)
-			printf("%d, ", i);
-			else if (i == 98)
-			printf("%d\n", i);
+			printf("%d", i);
+			printf(", ");
 		}
-		else if (n >= 98)
-		{
-			for (j = n; j >= 98; j--)
-			{
-				if (j != 98)
-					printf("%d, ", j);
-				else if (j == 98)
-					printf("%d\n", j);
-			}
-		}
+		printf("\n");
 	}
+}
