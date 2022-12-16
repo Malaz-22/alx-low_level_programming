2627af1f2be655ac7cf50b16ae88ff4b39100cce
Malaz Mohamed <113689938+Malaz-22@users.noreply.github.com>
2022-12-17 00:40:39 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,34 +1,47 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * print_square - prints a square
- *@size: print size
- * Return: Always 0.
+ * main - entry point
+ *
+ * Return: void
  */
 
-void print_square(int size)
+int main(void)
 {
 
+int p = 100;
 int i;
-int k;
 
-if (size > 0)
+i = 1;
+while (i <= p)
 {
-for (i = 0; i < size; i++)
+if (i % 3 == 0 && i % 5 == 0)
 {
-
-for (k = 0; k < size; k++)
-{
-_putchar (35);
-}
-_putchar ('\n');
-
+printf("FizzBuzz ");
 }
 
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+
+else if (i % 5 == 0)
+{
+if (i < p)
+printf("Buzz ");
+
+else
+printf("Buzz");
 }
 
 else
 {
-_putchar ('\n');
+printf("%i ", i);
 }
 
+i++;
+
+
+}
+printf("\n");
+return (0);
 }
