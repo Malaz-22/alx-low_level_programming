768091da938f1932fe2580295f23d9725da5ff80
Malaz-22 <malazm09@gmail.com>
2022-12-16 06:15:03 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,17 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	i = 0;
-	while (i < 10)
+	i = 1;
+	while (i <= 10)
 	{
 		j = 0;
 		while (j <= 14)
 		{
-			_putchar(j + '0');
-			j++;
+			if (j >= 10)
+			{
+				_putchar('1');
+				_putchar(j % 10 + '0');
+			}
 		}
 		_putchar('\n');
 		i++;
