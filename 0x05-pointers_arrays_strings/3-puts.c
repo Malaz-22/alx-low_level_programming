0d6c45000474c1e44ef5a20c75d21335894f7630
Malaz-22 <malazm09@gmail.com>
2022-12-20 15:39:46 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ * @str: points to char variable
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+}
