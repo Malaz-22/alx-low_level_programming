099a787ca943f6b095156abf1a1027acbc297b56
Malaz-22 <malazm09@gmail.com>
2022-12-22 13:06:52 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * strcmp - compares two integers
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 if s1 = s2 and greater than 0 if s1>s2
+ * else s1<s2
+ */
+ int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+	{
+		return (0);
+	}
+		s1++;
+		s2++;
+	}
+	return (*s1 -*s2);
+}
