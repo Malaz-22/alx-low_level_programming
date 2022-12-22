181fbf733ba89933b5acb10f4ea2f745c4b0ea32
Malaz-22 <malazm09@gmail.com>
2022-12-22 13:15:44 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,5 @@
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-<<<<<<< HEAD
 int _strcmp(char *s1, char *s2);
-=======
 char *_strncpy(char *dest, char *src, int n);
->>>>>>> 5de50c301a01e7e53722138f958feece36750295
