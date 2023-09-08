75aab515a210e2f97afa857d21d943d1f3567bc8
Malaz-22 <malazm09@gmail.com>
2023-09-07 18:45:54 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 to be correct
+ * no parameter
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
