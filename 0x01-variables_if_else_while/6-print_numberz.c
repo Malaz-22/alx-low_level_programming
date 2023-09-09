8e099bd1da121c85bc66ca9b8e34cf839aa1f256
Malaz-22 <malazm09@gmail.com>
2023-09-08 17:44:29 -0700

    putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 to be correct
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar('n' + '0');
+	}
+	putchar('\n');
+	return (0);
+}
