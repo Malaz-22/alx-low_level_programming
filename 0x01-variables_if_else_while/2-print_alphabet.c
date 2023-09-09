98ca97f1bae87eeb41afebbf3d91f9fbe54496db
Malaz-22 <malazm09@gmail.com>
2023-09-08 17:15:07 -0700

    lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 to be ok
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+		putchar('\n');
+	}
+	return (0);
+}
