961a9759cdbae7be8db22dc15e218f56130bca78
Malaz-22 <malazm09@gmail.com>
2023-09-08 19:40:37 -0700

    3comb

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 to be correct
+ */
+int main(void)
+{
+	int n, m, l;
+
+	for (n = 0; n <= 9; n++)
+	for (m = 0; m <= 9; m++)
+	for (l = 0; l <= 9; l++)
+	if ((n < m) && (n < l))
+	{
+		putchar(n + '0');
+		putchar(m + '0');
+		putchar(l + '0');
+	}
+	if (!((n == 7) && (m == 8) && (l == 9)))
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
