3d947c4f3005fb5798129695d7c275bf20466f16
Malaz-22 <malazm09@gmail.com>
2023-09-08 19:13:26 -0700

    2comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 to be correct
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n <= 9; n++)
+	for (m = 0; m <= 9; n++)
+	if (n < m)
+	{
+		putchar(n + '0');
+		putchar(m + '0');
+		if ((n == 8) && (m == 9))
+		{
+			putchar (',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
