39efc676275e3371bb95c5a897ccd25eb9e648ab
Malaz-22 <malazm09@gmail.com>
2023-09-08 17:36:20 -0700

    num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 to be correct
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num > 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
