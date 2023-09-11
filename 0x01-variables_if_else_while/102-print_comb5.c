d25f266704c00dfbc3f7b3470b221583eb260e1f
Malaz-22 <malazm09@gmail.com>
2023-09-11 16:18:56 -0700

    2digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,18 +11,19 @@ int main(void)
 	for (n = 0; n <= 99; n++)
 	for (m = 0; m <= 99; m++)
 	{
-		if (n != m)
+		if (n == m)
 		{
-			putchar(n / 10 + '0');
-			putchar(n % 10 + '0');
+			continue;
+		}
+		putchar(n / 10 + '0');
+		putchar(n % 10 + '0');
+		putchar(' ');
+		putchar((m + 1) / 10 + '0');
+		putchar((m + 1) % 10 + '0');
+		if (!((n == 98) && (m == 99)))
+		{
+			putchar(',');
 			putchar(' ');
-			putchar((m + 1) / 10 + '0');
-			putchar((m + 1) % 10 + '0');
-			if (!((n == 98) && (m == 99)))
-			{
-				putchar(',');
-				putchar(' ');
-			}
 		}
 	}
 	putchar('\n');
