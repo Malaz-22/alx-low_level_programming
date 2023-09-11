7523dc22650a925e6039f57632385571f81a76f7
Malaz-22 <malazm09@gmail.com>
2023-09-11 15:51:39 -0700

    2digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,20 +6,17 @@
  */
 int main(void)
 {
-	int n, m, l, q;
+	int n, m;
 
-	for (n = 0; n <= 9; n++)
-	for (m = 0; m <= 9; m++)
-	for (l = 0; l <= 9; l++)
-	for (q = 0; q <= 9; q++)
-	if ((n <= m) && (l < q))
+	for (n = 0; n <= 99; n++)
+	for (m = 0; m <= 99; m++)
 	{
-		putchar(n + '0');
-		putchar(m + '0');
+		putchar(n / 10 + '0');
+		putchar(n % 10 + '0');
 		putchar(' ');
-		putchar(l + '0');
-		putchar(q + '0');
-		if (!((n == 9) && (m == 8) && (l == 9) && (q == 9)))
+		putchar(m / 10 + '0');
+		putchar((m + 1) % 10 + '0');
+		if (!((n == 98) && (m == 99)))
 		{
 			putchar(',');
 			putchar(' ');
