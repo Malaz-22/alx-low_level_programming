23256c5dd97a3997fe77e03a21b0f3c1e3408983
Malaz-22 <malazm09@gmail.com>
2023-09-11 17:22:18 -0700

    5comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,12 +11,9 @@ int main(void)
 	for (n = 0; n <= 99; n++)
 	for (m = 0; m <= 99; m++)
 	{
-		if (n != m)
-		{
-			break;
-		}
-		putchar(n / 10 + '0');
-		putchar(n % 10 + '0');
+		if (((n / 10) && (n % 10)) != ((m + 1) / 10) && ((m + 1) % 10))
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
 		putchar(' ');
 		putchar((m + 1) / 10 + '0');
 		putchar((m + 1) % 10 + '0');
