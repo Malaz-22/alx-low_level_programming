adcee1703ddffe452d5705c37c40afbba645404d
Malaz-22 <malazm09@gmail.com>
2023-09-12 13:42:05 -0700

    alphapeta

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  prints the alphabet in lowercase, followed by a new line
+ * return: void (not exist)
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+}
+
