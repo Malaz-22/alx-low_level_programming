bc64a764c30e3c24ebab30623ecdfb52431c46d4
Malaz-22 <malazm09@gmail.com>
2023-09-14 09:52:05 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ * @n:number of \
+ */
+void print_diagonal(int n)
+{
+	int a;
+
+	if (n > 0)
+	{
+		for (a = 1; a <= n; a++)
+		{
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
