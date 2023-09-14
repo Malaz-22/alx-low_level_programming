ef6a2c946c839452ee8cc1e2be4145c1e558a1c6
Malaz-22 <malazm09@gmail.com>
2023-09-13 19:10:00 -0700

    sum

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,30 +11,12 @@ int main(void)
 	for (i = 3; i < 1024; i = +3)
 	for (j = 5; j < 1024; j = +5)
 	sum = i + j;
-	if (sum < 10)
-	{
-		printf("%d", sum);
-	}
-	if ((sum >= 10) && (sum < 100))
-	{
-		printf("%d\n", sum / 10);
-		printf("%d\n", sum % 10);
-	}
-	if ((sum >= 100) && (sum <= 999))
-	{
-		printf("%d", sum / 100);
-		printf("%d", (sum / 10) % 10);
-		printf("%d", sum % 10);
-	}
-	if (sum > 999)
-	{
-		printf("%d", sum / 1000);
-		printf("%d", (sum / 100) % 10);
-		printf("%d", (sum / 10) % 10);
-		printf("%d", sum % 10);
-	}
-	printf(",");
-	printf(" ");
+	printf("%d", sum / 100000);
+	printf("%d", (sum / 10000) % 10);
+	printf("%d", (sum / 1000) % 10);
+	printf("%d", (sum / 100) % 10);
+	printf("%d", (sum / 10) % 10);
+	printf("%d", sum % 10);
 	printf("\n");
 	return (sum);
 }
