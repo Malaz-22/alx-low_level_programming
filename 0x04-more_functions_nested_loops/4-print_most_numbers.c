1269b1f35bd80ade3b712f26781ee697e521c18e
Malaz-22 <malazm09@gmail.com>
2023-09-14 07:28:08 -0700

    printnum

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int m;
+
+	for (m = 0; m <= 9; m++)
+	{
+		if (m == 2 || m == 4)
+		{
+			break;
+		}
+		_putchar(m + '0');
+	}
+	_putchar('\n');
+}
