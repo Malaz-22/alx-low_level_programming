5bfc877aad86a8b472a392d9cc767ba949f5b719
Malaz-22 <malazm09@gmail.com>
2023-09-15 17:44:57 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,7 +10,7 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (j = 1; j <= i; j++)
+		for (j = 1; j <= size; j++)
 		{
 			for (i = 1; i <= size; i++)
 			{
@@ -19,5 +19,8 @@ void print_square(int size)
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
 }
