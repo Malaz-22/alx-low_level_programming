bb33cdd60da2e1e332fcb0f0b0d9c8f9a1451684
Malaz-22 <malazm09@gmail.com>
2023-09-18 16:21:24 -0700

    even

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to char
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	for (i = i - 1; s[i] >= 0; i--)
+	{
+	}
