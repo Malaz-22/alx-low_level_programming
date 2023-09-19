abe1539d622b986f7f2471641d66b4521d379b26
Malaz-22 <malazm09@gmail.com>
2023-09-18 17:56:01 -0700

    lasthalf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: point to char
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	if (i % 2 != 0)
+	{
+		for (i = (i - 1) / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	if (i % 2 == 0)
+	{
+		for (i = (i / 2); str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+}
