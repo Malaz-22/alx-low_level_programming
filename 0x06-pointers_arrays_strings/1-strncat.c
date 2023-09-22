b2e361bc68d06a6d3e134409ceb09099c31f0edc
Malaz-22 <malazm09@gmail.com>
2023-09-22 14:15:04 -0700

    strncpy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,10 +11,10 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; j != '\0' && j < n; j++, i++)
+	for (j = 0; src[j] != '\0' && j < n; j++, i++)
 	{
 		dest[i] = src[j];
 	}
