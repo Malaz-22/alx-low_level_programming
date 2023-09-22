2baff88d06ceff9762a578ec7f2cb6c517728261
Malaz-22 <malazm09@gmail.com>
2023-09-21 19:16:47 -0700

    rev

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array
+ * @n: num of elements
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	}
+	for (i = n - 1; a[i] >= 0; i--)
+	{
+	}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
