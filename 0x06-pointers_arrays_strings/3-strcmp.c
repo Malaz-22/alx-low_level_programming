8016ef063936f0e45283cbd5272f84e4e5e720b3
Malaz-22 <malazm09@gmail.com>
2023-09-21 18:51:37 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,17 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
-
-	for (i = 0; s1[i] != '\0'; i++)
+	while (*s1 == *s2)
 	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
 	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
-	if (i != j)
-	{
-		return (s1[i] - s2[j]);
-	}
-	return (0);
+	return (*s1 - *s2);
 }
