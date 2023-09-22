7a55cb26d159567be9e5b7844d2a0d890cb287e2
Malaz-22 <malazm09@gmail.com>
2023-09-22 13:39:50 -0700

    strncpy

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
