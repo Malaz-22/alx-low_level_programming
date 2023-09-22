bb48fdeb90b27e755eac7dbf2edbf580d6d5734b
Malaz-22 <malazm09@gmail.com>
2023-09-21 19:27:11 -0700

    rev

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,12 +7,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i;
+	int i, j, tmp;
 
-	for (i = 0; i < n; i++)
+	j = n - 1;
+	for (i = 0; i < n / 2; i++)
 	{
-	}
-	for (i = n - 1; a[i] >= 0; i--)
-	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j--] = tmp;
 	}
 }
