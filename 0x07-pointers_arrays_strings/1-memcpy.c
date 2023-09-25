561c59e2ff58e783ed2b122134bf7b34791e8fb3
Malaz Mohamed <113689938+Malaz-22@users.noreply.github.com>
2023-09-26 01:29:42 +0300

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: memory area (dest)
+ * @src: memory area (source)
+ * n: bytes will copied from memory area
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int c;
+
+	for (c = 0; c < n; c++)
+	{
+		dest[c] = src[c];
+	}
+
+	return (dest);
+}
