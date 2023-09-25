c789db4a98ab23bc5d6779b7c94ae65770495a4a
Malaz-22 <malazm09@gmail.com>
2023-09-25 15:33:06 -0700

    memcopy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,7 +4,7 @@
  * _memcpy - copies memory area
  * @dest: memory area (dest)
  * @src: memory area (source)
- * n: bytes will copied from memory area
+ * @n: bytes will copied from memory area
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
