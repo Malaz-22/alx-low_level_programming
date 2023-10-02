a25f4b0f9bc6bc2597f15fe0bad81a9ca2f077a5
Malaz-22 <malazm09@gmail.com>
2023-10-02 12:36:58 -0700

    static library

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number which will need square root
+ * Return: natural square root of a number
+ */
+int _sqrt_recursion(int n)
+{
+	int i;
+
+	if (n != (i * i))
+	{
+		return (-1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == (i * i))
+	{
+		return (i);
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,3 +4,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int _sqrt_recursion(int n);
