052a196a00ce3209b3a2eec8e7513e7894fd00e9
Malaz-22 <malazm09@gmail.com>
2023-10-02 13:46:03 -0700

    create static library

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
@@ -18,3 +21,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _atoi(char *s);
+
+#endif
+
