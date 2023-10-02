27cae760686ea93c885fa3146dea44c127c05078
Malaz-22 <malazm09@gmail.com>
2023-10-02 12:50:09 -0700

    static_lib

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
+/**
+ * main - entry point
+ * Return: 0
+ */
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t
+		    - Kevin Mitnick");
+	return (0);
 }
