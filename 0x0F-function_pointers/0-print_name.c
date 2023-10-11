f4ea5b8050cbcfc1066bdabe0a8685070f8543ff
Malaz-22 <malazm09@gmail.com>
2023-10-11 16:48:26 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,6 @@
  * print_name - prints a name
  * @name: name to print
  * @f: pointer to function which contain address of other function
- * @a: points to char values
  */
 void print_name(char *name, void (*f)(char *))
 {
