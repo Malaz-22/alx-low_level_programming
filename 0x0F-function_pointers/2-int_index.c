d54f02aff4c429bae36e2fe08c9188aad1ad7c80
Malaz-22 <malazm09@gmail.com>
2023-10-11 19:27:51 -0700

    index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,13 +3,13 @@
 /**
  * int_index - searches for an integer.
  * @array: point to int array
- * size: size of array
+ * @size: size of array
  * @cmp: function to pointer return int
  * Return: int value
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i, s;
 
 	if (size <= 0)
 		return (-1);
@@ -17,7 +17,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		for (i = 0; i < size; i++)
 		{
-			cmp(array[i]);
+			s = cmp(array[i]);
+			if (s != 0)
+			{
+				return (i);
+			}
 		}
 	}
 	return (-1);
