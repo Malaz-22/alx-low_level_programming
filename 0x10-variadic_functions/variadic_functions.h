ee80a3be7891b25a6a96550626dfa7b0d485998f
Malaz Mohamed <113689938+Malaz-22@users.noreply.github.com>
2023-10-13 04:32:05 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,6 @@
 #ifndef VARIADIC_FUN_H
 #define VARIADIC_FUN_H
-
+#include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 int _putchar(char c);
