62ef3c1ea313996e9c683a064d3d0fb9c67a613e
Malaz-22 <malazm09@gmail.com>
2023-10-24 14:39:20 -0700

    argc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,9 +10,12 @@ int main(int argc, char *argv[] __attribute__((unused)))
 {
 	int count;
 
-	for (count = 1; count < argc; count++)
+	if (argc > 1)
 	{
-		printf("%d\n", count);
+		for (count = 1; count < argc; count++)
+		{
+			printf("%d\n", count);
+		}
 	}
 	return (0);
 }
