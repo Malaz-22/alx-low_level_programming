4a9c05becaf5c4c612b68f6d896eb0e0a61def89
Malaz-22 <malazm09@gmail.com>
2023-10-28 12:25:08 -0700

    dup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,24 +8,15 @@
  */
 char *_strdup(char *str)
 {
-	char *dup;
-	unsigned int i, len;
+	int i;
 
-	i = 0;
-	len = 0;
-
+	for (i = 0; i != '\0'; i++)
+	{
+	}
+	str = malloc(sizeof(*str) * i);
 	if (str == NULL)
+	{
 		return (NULL);
-
-	while (str[len])
-		len++;
-	dup = malloc(sizeof(char) * (len + 1));
-
-	if (dup == NULL)
-		return (NULL);
-
-	while ((dup[i] = str[i]) != '\0')
-		i++;
-
-	return (dup);
+	}
+	return (str);
 }
