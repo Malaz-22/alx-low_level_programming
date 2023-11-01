994c488559e9de0ecfe3c08c73234646228e7770
Malaz-22 <malazm09@gmail.com>
2023-11-01 12:42:13 -0700

    fun_like_nacro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) x
-#if (x < 0)
-printf("%d\n", -x);
-#endif /* x < 0 */
-#else
-printf("%d\n", x);
+#define ABS(x) ((x) < (0) ? (x) : (-x))
 
-#endif
+#endif/* #ifndef FUNCTION_LIKE_MACRO_H */
